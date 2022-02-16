cea6609dca3924ceaaf8c562b01b6b37dd3ec274
Sami Ojanpää <sami.ojanpaa@cgi.com>
2022-02-16 16:20:41 +0200

    script for vscode extensions

---
+++ install-vscode-extensions.sh
@@ -0,0 +1,9 @@
+code --install-extension vscjava.vscode-java-pack
+code --install-extension pivotal.vscode-boot-dev-pack
+code --install-extension redhat.vscode-xml
+code --install-extension dcortes92.freemarker
+code --install-extension sodatea.velocity
+code --install-extension eamodio.gitlens
+code --install-extension asciidoctor.asciidocvscode
+code --install-extension humao.rest-client
+code --install-extension redhat.fabric8-analytics
